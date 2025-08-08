39cbeb8580df3209722ef4e8bc12f53ee93b589c
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2025-08-08 18:44:09 -0400

    scripts sql para la utilizacion de H2

---
+++ UserClientGlBci/src/test/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (name, email, password, phones) VALUES (
+    'Juan Pérez',
+    'juan.perez@example.com',
+    'password123',
+    '[{"number": 123456789, "citycode": 1, "contrycode": "ES"}, {"number": 987654321, "citycode": 2, "contrycode": "US"}]'
+);
+
+INSERT INTO users (name, email, password, phones) VALUES (
+    'María Gómez',
+    'maria.gomez@example.com',
+    'password456',
+    '[{"number": 555555555, "citycode": 3, "contrycode": "MX"}]'
+);
