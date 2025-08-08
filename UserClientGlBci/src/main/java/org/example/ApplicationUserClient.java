a9e1d0ba457395e98657b894956d1ad74776bc80
RodrigoIsaacHerrera <rodrigoherrerabeiza01@gmail.com>
2025-08-08 18:13:15 -0400

    preparando ambiente para comenzar TDD

---
+++ UserClientGlBci/src/main/java/org/example/ApplicationUserClient.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationUserClient {
+    public static void main(String[] args) {
+
+    }
+}
